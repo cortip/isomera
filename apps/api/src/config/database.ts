c799e4093d6e722f265fb0577a056030d56020e5
Cristofer Portela <cristofer.portela@neomind.com.br>
2022-10-27 22:40:33 -0300

    refact: datbase configuration changed

--- apps/api/src/config/database.ts
+++ apps/api/src/config/database.ts
@@ -1,39 +1,29 @@
 import { join } from 'path';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
 import { DataSource } from 'typeorm';
 
 export const dbConfigPg = (): PostgresConnectionOptions => ({
-  type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  ssl: false,
-  entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
-  // We are using migrations, synchronize should be set to false.
+  type: process.env.TYPEORM_DRIVER as never,
+  host: process.env.TYPEORM_HOST,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  port: Number(process.env.TYPEORM_PORT),
   synchronize: false,
-  dropSchema: false,
-  // Run migrations automatically,
-  // you can disable this if you prefer running migration manually.
-  migrationsRun: true,
-  logging: false,
-  migrations: [join(__dirname, '../migrations/**/*{.ts,.js}')],
+  entities: [join(__dirname, "../**/entities/*.entity.ts")],
+  migrations: [join(__dirname, '../migrations/*{.ts,.js}')]
 });
 
-export const dbConfigDev = (): SqliteConnectionOptions => ({
-  type: 'sqlite',
-  database: 'isomera_dev',
-  entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
-  // We are using migrations, synchronize should be set to false.
-  synchronize: false,
-  dropSchema: false,
-  // Run migrations automatically,
-  // you can disable this if you prefer running migration manually.
-  migrationsRun: true,
-  logging: true,
-  migrations: [join(__dirname, '../migrations/**/*{.ts,.js}')],
+export const dbConfigDev = () => ({
+  type: process.env.TYPEORM_DRIVER as never,
+  host: process.env.TYPEORM_HOST,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  port: Number(process.env.TYPEORM_PORT),
+  synchronize: process.env.TYPEORM_SYNCRONIZE === "true",
+  entities: [join(__dirname, "../**/entities/*.entity.ts")],
+  migrations: [join(__dirname, '../migrations/*{.ts,.js}')]
 });
 
 export default new DataSource(dbConfigPg());
