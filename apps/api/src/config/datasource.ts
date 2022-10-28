613b2fa5d637953ed87bd0fa3c5330eeacfa65b0
Cristofer Portela <cristofer.portela@neomind.com.br>
2022-10-28 12:52:28 -0300

    fix:  importing configuration

--- apps/api/src/config/datasource.ts
+++ apps/api/src/config/datasource.ts
@@ -1,22 +1,11 @@
 import dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
-import { join } from 'path';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { dbConfigPg } from './database';
 
 // Load env file
 dotenv.config();
 
-const AppDataSource = new DataSource({
-  type: process.env.TYPEORM_DRIVER as PostgresConnectionOptions['type'],
-  host: process.env.TYPEORM_HOST,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-  port: Number(process.env.TYPEORM_PORT),
-  synchronize: process.env.TYPEORM_SYNCRONIZE === 'true',
-  entities: [join(__dirname, '../**/entities/*.entity.ts')],
-  migrations: [join(__dirname, '../migrations/*{.ts,.js}')]
-});
+const AppDataSource = new DataSource(dbConfigPg());
 
 AppDataSource.initialize()
   .then(() => {
