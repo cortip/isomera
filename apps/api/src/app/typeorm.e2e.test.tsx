5f65d78a37093a9f1ba2f88a04c178b069d2d7cf
Cristofer Portela <cristoferportela@gmail.com>
2022-11-03 15:04:42 -0300

    test: database connection testing created

---
+++ apps/api/src/app/typeorm.e2e.test.tsx
@@ -0,0 +1,40 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from './app.module';
+import { Connection, DataSource } from 'typeorm';
+import { User } from '../user/entities/user.entity';
+
+let app;
+let connection;
+
+beforeAll(async () => {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule]
+  }).compile();
+
+  app = moduleFixture.createNestApplication();
+  await app.init();
+
+  connection = app.get(Connection);
+  await connection.synchronize(true);
+
+});
+
+test('It should generate Connection object', async () => {
+
+  expect(connection).toBeInstanceOf(DataSource);
+
+});
+
+test('It should create the user', async () => {
+  const result = await connection.createQueryBuilder().insert().into(User).values([{
+    name: 'John Doe',
+    email: 'JohnDoe@email.com',
+    password: '12345678'
+  }]).execute();
+
+  expect(result).not.toBeNull();
+})
+
+afterAll(async () => {
+  await app.close();
+});
