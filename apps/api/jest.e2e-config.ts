e9ee9f934dd3a41880e02414e331b09586b37d2e
Cristofer Portela <cristoferportela@gmail.com>
2022-11-06 16:26:50 -0300

    test|chore: includes e2e

---
+++ apps/api/jest.e2e-config.ts
@@ -0,0 +1,45 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
+  displayName: 'api',
+  preset: '../../jest.preset.js',
+  testNamePattern: '^.+\\.e2e\.test\.[tj]s$',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'html', 'json'],
+  coverageDirectory: '../../coverage/apps/api',
+  setupFiles: [
+    'dotenv',
+    'jest-ts-auto-mock',
+    '<rootDir>jest-ts-auto-mock.config.ts',
+  ],
+  globals: {
+    'ts-jest': {
+      compiler: 'ttypescript',
+    },
+  },
+
+  // rootDir: 'src',
+  // testRegex: '.spec.ts$',
+  // transform: {
+  //   '^.+\\.ts$': 'ts-jest',
+  // },
+  // coverageDirectory: '../coverage',
+  // testEnvironment: 'node',
+  // setupFiles: ['@suncin/dotenv', 'jest-ts-auto-mock'],
+  // globals: {
+  //   'ts-jest': {
+  //     compiler: 'ttypescript',
+  //     tsconfig: 'tsconfig.test.json',
+  //   },
+  // },
+};
+
+export default jestConfig;
