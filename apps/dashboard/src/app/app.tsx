d124c9731d57b53e12d9d09ab29b093e0727fc8d
Cristofer Portela <cristoferportela@gmail.com>
2022-11-07 19:28:30 -0300

    feat: added toast message container

--- apps/dashboard/src/app/app.tsx
+++ apps/dashboard/src/app/app.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Route, Routes } from 'react-router-dom';
 import { Login } from './pages/login';
 import { Register } from './pages/register';
+import { ToastContainer } from 'react-toastify';
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      <ToastContainer />
     </StyledApp>
   );
 };
