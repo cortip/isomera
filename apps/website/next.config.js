34ae3af207788c903b2a123dfe280cc043d732c5
Vygandas Pliasas <vygandas.pliasas@gmail.com>
2022-11-07 11:48:38 +0200

    with-nx override

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,5 +1,17 @@
+/**
+ * Restore back to the following import when issues are fixed:
+ * const { withNx } = require('@nrwl/next/plugins/with-nx');
+ *
+ * Issues:
+ * https://github.com/nrwl/nx/pull/13006
+ * https://github.com/nrwl/nx/pull/12973
+ * https://github.com/nrwl/nx/pull/12973/commits/3936b6e6554524433bd552f5f102765fffad5b3c
+ *
+ * Also remove temporary file.
+ */
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withNx } = require('@nrwl/next/plugins/with-nx');
+const { withNx } = require('./with-nx');
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
