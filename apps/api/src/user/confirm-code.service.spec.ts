4a1286f805d7fceee27b86f235bf7e587e5700bc
Cristofer Portela <cristoferportela@gmail.com>
2022-11-14 11:13:43 -0300

    test: basic test created

---
+++ apps/api/src/user/confirm-code.service.spec.ts
@@ -0,0 +1,38 @@
+import { Test, type TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { createMock } from 'ts-auto-mock';
+import type { Repository } from 'typeorm';
+
+import { User } from './entities/user.entity';
+import { UserService } from './user.service';
+import { ConfirmCodeService } from './confirm-code.service';
+import { ConfirmCode } from './entities/confirm-code.entity';
+
+describe('ConfirmCodeService', () => {
+  let service: ConfirmCodeService;
+  let mockedConfirmCodeRepository: jest.Mocked<Repository<ConfirmCode>>;
+  let mockedUserRepository: jest.Mocked<Repository<User>>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ConfirmCodeService],
+    })
+      .useMocker((token) => {
+        if (Object.is(token, getRepositoryToken(User))) {
+          return createMock<Repository<User>>();
+        }
+        if (Object.is(token, getRepositoryToken(ConfirmCode))) {
+          return createMock<Repository<ConfirmCode>>();
+        }
+      })
+      .compile();
+
+    service = module.get<ConfirmCodeService>(ConfirmCodeService);
+    mockedUserRepository = module.get(getRepositoryToken(User));
+    mockedConfirmCodeRepository = module.get(getRepositoryToken(ConfirmCode));
+  });
+
+  it('should be an instanceof ConfirmCodeService', () => {
+    expect(service).toBeInstanceOf(ConfirmCodeService);
+  });
+});
