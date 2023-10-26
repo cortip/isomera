392c8134365713966bb0e9ad813bfa7ab3318029
Vygandas Pliasas <vygandas.pliasas@gmail.com>
2023-10-26 08:46:08 +0300

    touch all apps to force deployments no 2

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,14 +8,15 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
-//
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
--- apps/platform/src/app/app.tsx
+++ apps/platform/src/app/app.tsx
@@ -5,8 +5,6 @@ import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
 
-//
-
 export function App() {
   return (
     <div>
