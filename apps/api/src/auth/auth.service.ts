7f3bc916f7376c5e34f7771380d3ae6168dd4654
naftalimurgor <murgornaftali@gmail.com>
2023-12-08 18:11:02 +0300

    refactor: minor refactor, set `user.password` to `undefined` in `auth.service`

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -25,13 +25,13 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly mailerService: MailerService,
     private readonly confirmCode: ConfirmCodeService
-  ) {}
+  ) { }
 
   async register(
     SignUpWithEmailCredentialsDto: Pure<SignUpWithEmailCredentialsDto>
   ): Promise<UserEntity> {
     const user = await this.userService.create(SignUpWithEmailCredentialsDto)
-    delete user.password
+    user.password = undefined
 
     //TODO: mock confirmCode
     if (process.env.NODE_ENV !== 'test') {
@@ -73,7 +73,7 @@ export class AuthService {
         `Wrong password for user with email: ${email}`
       )
     }
-    delete user.password
+    user.password = undefined
 
     return user
   }
@@ -88,7 +88,7 @@ export class AuthService {
         `There isn't any user with email: ${payload.sub}`
       )
     }
-    delete user.password
+    user.password = undefined
 
     return user
   }
