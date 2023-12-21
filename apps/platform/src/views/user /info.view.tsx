671836095fb3f906dfc4c05eb79489cb31673f1d
HoangPham <31430555+PhamAnhHoang@users.noreply.github.com>
2023-12-21 17:02:58 +0700

    Update logout feature

--- apps/platform/src/views/user /info.view.tsx
+++ apps/platform/src/views/user /info.view.tsx
@@ -1,12 +1,29 @@
-import { useUserHook } from '@isomera/impl'
+import { useLogoutPerformForm, useUserHook } from '@isomera/impl'
+import { toast } from 'react-toastify'
 
 export const UserInfoView = () => {
   const { data, isFetched } = useUserHook()
+
+  const onSuccess = (message: string) => {
+    toast.success(message)
+  }
+
+  const onError = (message: string) => {
+    toast.error(message)
+  }
+
+  const { handleClick } = useLogoutPerformForm(onSuccess, onError)
+
   return (
     <div>
       Profile here
       <div>First Name: {data?.firstName}</div>
       <div>Last Name: {data?.lastName}</div>
+      <div>
+        <button type="button" onClick={handleClick}>
+          Logout
+        </button>
+      </div>
     </div>
   )
 }
---
+++ libs/impl/src/hooks/auth/useLogoutPerformForm.hook.ts
@@ -0,0 +1,36 @@
+import { useNavigate } from 'react-router-dom'
+
+import { pages } from '../../constants/pages'
+import { useHandleErrorHook } from '../error/useHandleError.hook'
+import { StatusType } from '@isomera/interfaces'
+import { toast } from 'react-toastify'
+import { userLogoutPerformHook } from './useLogoutPerform.hook'
+import { clearAuthState } from '../../handlers'
+
+export const useLogoutPerformForm = (
+  onSuccess: (arg0: string) => void,
+  onError: (arg0: string) => void
+) => {
+  const { performReset } = userLogoutPerformHook()
+  const { handleError } = useHandleErrorHook()
+  const navigate = useNavigate()
+
+  const handleClick = async () => {
+    try {
+      const result = await performReset()
+      if (result.status === StatusType.OK) {
+        clearAuthState()
+        onSuccess('Logout successfully.')
+      } else {
+        onError('Logout failed. Please try again.')
+      }
+      navigate(pages.login.path)
+    } catch (error) {
+      handleError(error, { view: 'login' })
+    }
+  }
+
+  return {
+    handleClick
+  }
+}
