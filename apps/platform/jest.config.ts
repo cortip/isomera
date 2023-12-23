c20fa84f3ba81600cc99c0be99786d25c03bcbec
padi-dev-hoangpa <31430555+PhamAnhHoang@users.noreply.github.com>
2023-12-23 11:42:46 +0700

    lint

--- apps/platform/jest.config.ts
+++ apps/platform/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/platform',
+  coverageDirectory: '../../coverage/apps/platform'
 }
